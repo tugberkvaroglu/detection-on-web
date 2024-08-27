44cb79079d4443968e27b07c62e01594e9ebe623
tugberk.varoglu <tugberkvaroglu03@gmail.com>
2024-08-27 23:43:26 +0300

    Changed navbar

--- object-detection-app/src/App.js
+++ object-detection-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import UploadImage from './UploadImage';
+import './MainPage.css'
 
 function App() {
     return (
         <div className="App">
+            <nav className='top-nav-bar'>
+                <button className='first-model-button' >First Model</button>
+                <button className='second-model-button' >Second Model</button>
+                <text className='nav-bar-title'>Upload Image</text>
+            </nav>
             <UploadImage />
         </div>
     );
